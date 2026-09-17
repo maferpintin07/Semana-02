714260a82e9ac7dd795e4bc0cdb1c1d25fe70366
mafer <maferpintin07@gmail.com>
2026-09-16 22:22:20 -0600

    Commit 2: Trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 # Esta sesión será para aprender el uso de variables
-print("Hola mundo")
-print("Hoy es miércoles")
-print("Mañana es jueves")
+print("Sistema de mensajería")
+distancia_km = 40.7
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+
+distancia_km = distancia_km + 16
+print(distancia_km)
